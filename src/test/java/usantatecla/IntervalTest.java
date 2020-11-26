28b9f1cf31cb83cada1a30f04e6f160db21335cb
Setillo <setillofm@gmail.com>
2020-11-26 09:31:46 +0100

    red

--- src/test/java/usantatecla/IntervalTest.java
+++ src/test/java/usantatecla/IntervalTest.java
@@ -1,5 +1,6 @@
 package usantatecla;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -9,6 +10,11 @@ public class IntervalTest {
   @Test
   public void givenIntervalwhenIncludeWithIncludedValueThenTrue(){
     assertTrue(new Interval(-1.7,5555.0).include(0.0));
+  }
+
+  @Test
+  public void givenIntervalwhenIncludeWithNotIncludedValueThenFalse(){
+    assertFalse(new Interval(-1.7,5555.0).include(9000.0));
   }
   
 }
