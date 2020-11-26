d07ae86360dade87c114257cdf364858b9d45bf3
Setillo <setillofm@gmail.com>
2020-11-26 09:56:55 +0100

    refactor, green

--- src/test/java/usantatecla/MaxTest.java
+++ src/test/java/usantatecla/MaxTest.java
@@ -1,5 +1,6 @@
 package usantatecla;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -7,8 +8,13 @@ import org.junit.jupiter.api.Test;
 public class MaxTest {
    
   @Test
-  public void givenIntervalwhenIncludeWithIncludedValueThenTrue(){
+  public void givenMaxwhenGreaterWithLessValueThenTrue(){
     assertTrue(new Max(true, 4).greater(0.0));
+  }
+
+  @Test
+  public void givenMaxwhenGreaterWithLessValueThenFalse(){
+    assertFalse(new Max(true, 4).greater(5.0));
   }
  
 }
