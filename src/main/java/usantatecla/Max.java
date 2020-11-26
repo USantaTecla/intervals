3a840c19fff77ddfdf66449a56b79c9ca5ed6cbb
Setillo <setillofm@gmail.com>
2020-11-26 10:02:10 +0100

    refactor, green

--- src/main/java/usantatecla/Max.java
+++ src/main/java/usantatecla/Max.java
@@ -2,11 +2,19 @@ package usantatecla;
 
 public class Max {
 
+  private double value;
+  private boolean open;
+
 	public Max(boolean b, int i) {
-	}
+    this.value = i;
+    this.open = b;
+  }
 
 	public boolean greater(double d) {
-		return true;
+    if (this.open){
+      return this.value > d;
+    }
+    return this.value >= d;
 	}
   
 }
