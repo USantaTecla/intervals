34bfcde08462922b124eeab8bd5df9150044a1b1
Setillo <setillofm@gmail.com>
2020-11-26 09:33:48 +0100

    green

--- src/main/java/usantatecla/Interval.java
+++ src/main/java/usantatecla/Interval.java
@@ -2,11 +2,16 @@ package usantatecla;
 
 public class Interval {
 
+	private double min;
+	private double max;
+
 	public Interval(double d, double e) {
+		this.min = d;
+		this.max = e;
 	}
 
 	public boolean include(double d) {
-		return true;
+		return this.min <= d && d <= this.max;
 	}
 
 
