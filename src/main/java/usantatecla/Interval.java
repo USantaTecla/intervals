4d1a838fb42df533bf6fe7b6a70490b074a826d4
Setillo <setillofm@gmail.com>
2020-11-26 09:45:22 +0100

    green

--- src/main/java/usantatecla/Interval.java
+++ src/main/java/usantatecla/Interval.java
@@ -4,14 +4,19 @@ public class Interval {
 
 	private double min;
 	private double max;
+	private boolean maxOpen;
 
 	public Interval(boolean minOpen, double min, boolean maxOpen, double max) {
 		assert min <= max;
 		this.min = min;
+		this.maxOpen = maxOpen; 
 		this.max = max;
 	}
 
 	public boolean include(double value) {
+		if (this.maxOpen){
+			return this.min <= value && value < this.max;
+		}
 		return this.min <= value && value <= this.max;
 	}
 
