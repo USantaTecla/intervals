76bd08134023ebc2904dacdab9bb5c0249642eed
Setillo <setillofm@gmail.com>
2020-11-26 09:35:18 +0100

    refactor

--- src/main/java/usantatecla/Interval.java
+++ src/main/java/usantatecla/Interval.java
@@ -5,14 +5,14 @@ public class Interval {
 	private double min;
 	private double max;
 
-	public Interval(double d, double e) {
-		this.min = d;
-		this.max = e;
+	public Interval(double min, double max) {
+		assert min <= max;
+		this.min = min;
+		this.max = max;
 	}
 
-	public boolean include(double d) {
-		return this.min <= d && d <= this.max;
+	public boolean include(double value) {
+		return this.min <= value && value <= this.max;
 	}
-
 
 }
